997d3a0306933d5e3928be03323d3944391ff20d
Jakkaphat <gunt@guntxjakka.me>
2022-04-11 05:00:22 +0700

    fix: keyboard layout error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const prefix = config.prefix;
 const token = config.token;
 
 function gode(text) {
-    let ans = godejs.convert(text, "QWERTY", "Kedmanee")
+    let ans = godejs.convert("QWERTY", "Kedmanee", message)
     return ans
 }
 
